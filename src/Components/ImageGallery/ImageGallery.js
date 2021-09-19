0e0dcdaac26a093b4a8799810e54be95ec70fef3
Kostiantyn Stoliarskyi <saintkos117@gmail.com>
2021-09-19 15:42:15 +0300

    done

--- src/Components/ImageGallery/ImageGallery.js
+++ src/Components/ImageGallery/ImageGallery.js
@@ -4,11 +4,57 @@ import ImageGalleryItem from '../ImageGalleryItem/ImageGalleryItem';
 import s from './ImageGallery.module.css';
 import Button from '../Button/Button';
 import Modal from '../Modal/Modal';
+import api from '../../services/imgAPI';
 export default class ImageGallery extends Component {
   state = {
     page: 1,
     modalUrl: '',
     modalIsOpen: false,
+    response: [],
+    error: null,
+  };
+  componentDidMount() {
+    const list = document.querySelector('ul');
+    const { openModal } = this.props;
+    this.fetchImagesByName();
+    list.addEventListener('click', openModal);
+  }
+  componentDidUpdate(prevProps, prevState) {
+    const { search, scroll } = this.props;
+    const { page } = this.state;
+    if (prevProps.search !== search) {
+      this.setState({
+        response: [],
+      });
+      this.fetchImagesByName();
+    }
+    if (prevState.page !== page) {
+      this.fetchImagesByName().then(scroll);
+    }
+  }
+  componentWillUnmount() {
+    const list = document.querySelector('ul');
+    const { openModal } = this.props;
+    list.removeEventListener('click', openModal);
+  }
+  fetchImagesByName = () => {
+    const API_KEY = '21885958-186cb9f8de90f78c5ca194f62';
+    const { search, page } = this.props;
+    const errorMessage = `Изображений по ключевому слову ${search} не найдено`;
+    return api
+      .fetchImg(search, page)
+      .then(data => {
+        if (data.hits.length === 0) {
+          return Promise.reject(new Error(errorMessage));
+        }
+        return this.setState(prevState => ({
+          response: [...prevState.response, ...data.hits],
+        }));
+      })
+      .catch(error => alert(error.message))
+      .finally(() => {
+        this.props.hideLoader();
+      });
   };
   scroll = () => {
     window.scrollTo({
@@ -48,13 +94,27 @@ export default class ImageGallery extends Component {
       <>
         <div>
           <ul className={s.ImageGallery}>
-            <ImageGalleryItem
-              openModal={this.openModal}
-              hideLoader={hideLoader}
-              search={search}
-              page={page}
-              scroll={this.scroll}
-            />
+            {this.state.response.map(item => {
+              const {
+                id,
+                webformatURL,
+                tags,
+                largeImageURL,
+              } = item;
+              return (
+                <ImageGalleryItem
+                  id={id}
+                  webformatURL={webformatURL}
+                  tags={tags}
+                  largeImageURL={largeImageURL}
+                  openModal={this.openModal}
+                  hideLoader={hideLoader}
+                  search={search}
+                  page={page}
+                  scroll={this.scroll}
+                />
+              );
+            })}
           </ul>
         </div>
         <Button onClick={this.increasePage} />
@@ -69,6 +129,9 @@ export default class ImageGallery extends Component {
   }
 }
 ImageGallery.propTypes = {
+  hideLoader: PropTypes.func.isRequired,
+  openModal: PropTypes.func.isRequired,
+  page: PropTypes.number.isRequired,
   hideLoader: PropTypes.func.isRequired,
   search: PropTypes.string.isRequired,
 };
--- src/Components/ImageGalleryItem/ImageGalleryItem.js
+++ src/Components/ImageGalleryItem/ImageGalleryItem.js
@@ -1,73 +1,30 @@
 import { Component } from 'react';
 import PropTypes from 'prop-types';
 import s from './ImageGalleryItem';
-import api from '../../services/imgAPI';
+
 export default class ImageGalleryItem extends Component {
-  state = { response: [], error: null };
-  componentDidMount() {
-    const list = document.querySelector('ul');
-    const { openModal } = this.props;
-    this.fetchImagesByName();
-    list.addEventListener('click', openModal);
-  }
-  componentDidUpdate(prevProps, prevState) {
-    const { search, page, scroll } = this.props;
-    if (prevProps.search !== search) {
-      this.setState({
-        response: [],
-      });
-      this.fetchImagesByName();
-    }
-    if (prevProps.page !== page) {
-      this.fetchImagesByName().then(scroll);
-    }
-  }
-  componentWillUnmount() {
-    const list = document.querySelector('ul');
-    const { openModal } = this.props;
-    list.removeEventListener('click', openModal);
-  }
-  fetchImagesByName = () => {
-    const API_KEY = '21885958-186cb9f8de90f78c5ca194f62';
-    const { search, page } = this.props;
-    const errorMessage = `Изображений по ключевому слову ${search} не найдено`;
-    return api
-      .fetchImg(search, page)
-      .then(data => {
-        if (data.hits.length === 0) {
-          return Promise.reject(new Error(errorMessage));
-        }
-        return this.setState(prevState => ({
-          response: [...prevState.response, ...data.hits],
-        }));
-      })
-      .catch(error => alert(error.message))
-      .finally(() => {
-        this.props.hideLoader();
-      });
-  };
   render() {
-    return this.state.response.map(item => {
-      const { id, webformatURL, tags, largeImageURL } =
-        item;
-      return (
-        <li key={id} className={s.ImageGalleryItem}>
-          <a>
-            <img
-              src={webformatURL}
-              alt={tags}
-              data-big_image={largeImageURL}
-              className={s.ImageGalleryItem_image}
-            />
-          </a>
-        </li>
-      );
-    });
+    const { id, webformatURL, tags, largeImageURL } =
+      this.props;
+    return (
+      <li key={id} className={s.ImageGalleryItem}>
+        <a>
+          <img
+            src={webformatURL}
+            alt={tags}
+            data-big_image={largeImageURL}
+            className={s.ImageGalleryItem_image}
+          />
+        </a>
+      </li>
+    );
   }
 }
+
 ImageGalleryItem.propTypes = {
-  hideLoader: PropTypes.func.isRequired,
-  openModal: PropTypes.func.isRequired,
-  page: PropTypes.number.isRequired,
   search: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+  webformatURL: PropTypes.string,
+  tags: PropTypes.string,
+  largeImageURL: PropTypes.string,
 };
