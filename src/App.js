aab372b96f25d09ccfdeef0d810127f7ba8a2170
Kostiantyn Stoliarskyi <saintkos117@gmail.com>
2021-09-19 11:36:52 +0300

    правки

--- src/App.js
+++ src/App.js
@@ -43,7 +43,7 @@ export default class App extends Component {
         <div>
           <Searchbar
             onSubmit={this.handleSubmit}
-          ></Searchbar>
+          />
           {loaderAreShown && <Loaders />}
           {!searchCheck && (
             <ImageGallery
